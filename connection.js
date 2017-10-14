66589dfa5403a7a94d32a90467e74173bfabfd6c
saveryanov <railtrailtime@gmail.com>
2017-10-14 15:30:04 +0300

    first commit

---
+++ connection.js
@@ -0,0 +1,10 @@
+﻿var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+  if(!err) {
+    console.log("mongodb connected");
+  } else {
+    console.log("mongodb not connected");
+  }
+});
