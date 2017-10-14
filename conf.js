294ef0a87799f28696ca73dde21eb1f0e9cddf0a
saveryanov <railtrailtime@gmail.com>
2017-10-14 22:20:39 +0300

    add username and pass to config

--- conf.js
+++ conf.js
@@ -2,11 +2,22 @@
 var host = "localhost";		// хост
 var port = "27017";			// порт
 var dbname = "exampleDb";	// имя базы данных
+var username = "";
+var password = "";
 
 // Экспорт настроек
 module.exports.host = host;
 module.exports.port = port;
 module.exports.dbname = dbname;
 module.exports.getConnectionURI = function() {	// функция собирающая URI для подключения к БД
-	return "mongodb://" + host + ":" + port + "/" + dbname;
+	// Общий вид:
+	// mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
+	var uri = "";
+	uri += "mongodb://";
+	if(username && password) {	// если есть логин и пароль то добавляем их в URI (опционально)
+		uri += username + ":" + password + "@";
+	}
+	uri += host + ":" + port + "/" + dbname;	// добавляем хост, порт, имя базы
+	return uri;
 }
+
